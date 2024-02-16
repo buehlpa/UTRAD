92d2a12d63e38c98680f9a3bff83cc0c8e72a765
Pascal <buehler1991@gmail.com>
2024-02-16 09:31:32 +0100

    added contam plots

---
+++ training_scripts/Exp_15_02_24_extended.sh
@@ -0,0 +1,87 @@
+#!/bin/bash  Exp_15_02_24_extended
+
+python main.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.02
+python main.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.04
+python main.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.06
+python main.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.08
+python main.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.02
+python main.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.04
+python main.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.06
+python main.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.08
+python main.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.02
+python main.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.04
+python main.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.06
+python main.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.08
+python main.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+python valid.py --exp_name Exp_15_02_24 --data_category bottle --mode mvtec --contamination_rate 0.0 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category bottle --mode mvtec --contamination_rate 0.02 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category bottle --mode mvtec --contamination_rate 0.04 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category bottle --mode mvtec --contamination_rate 0.06 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category bottle --mode mvtec --contamination_rate 0.08 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category bottle --mode mvtec --contamination_rate 0.1 --unalign_test
+
+python valid.py --exp_name Exp_15_02_24 --data_category cable --mode mvtec --contamination_rate 0.0 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category cable --mode mvtec --contamination_rate 0.02 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category cable --mode mvtec --contamination_rate 0.04 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category cable --mode mvtec --contamination_rate 0.06 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category cable --mode mvtec --contamination_rate 0.08 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category cable --mode mvtec --contamination_rate 0.1 --unalign_test
+
+python valid.py --exp_name Exp_15_02_24 --data_category toothbrush --mode mvtec --contamination_rate 0.0 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category toothbrush --mode mvtec --contamination_rate 0.02 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category toothbrush --mode mvtec --contamination_rate 0.04 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category toothbrush --mode mvtec --contamination_rate 0.06 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category toothbrush --mode mvtec --contamination_rate 0.08 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category toothbrush --mode mvtec --contamination_rate 0.1 --unalign_test
+
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.0 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.02 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.04 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.06 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.08 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.1 --unalign_test
+
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.02 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.04 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.06 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.08 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.1 --unalign_test
+
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.0 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.02 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.04 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.06 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.08 --unalign_test
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.1 --unalign_test
+
+
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.0 
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.02 
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.04 
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.06 
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.08 
+python valid.py --exp_name Exp_15_02_24 --data_category leather --mode mvtec --contamination_rate 0.1 
+
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_15_02_24 --data_category metal_nut --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.0 
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.02 
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.04 
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.06 
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.08 
+python valid.py --exp_name Exp_15_02_24 --data_category screw --mode mvtec --contamination_rate 0.1 
