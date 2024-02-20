6e2abc73be0890d8df65691e45809426fa8a0f0d
Pascal <buehler1991@gmail.com>
2024-02-20 11:51:06 +0100

    fixed Benatec bug

--- datasets.py
+++ datasets.py
@@ -26,9 +26,9 @@ _pil_interpolation_to_str = {
 }
 #*2
 
-# TODO loader for, visa , beantec
+# TODO loader for, visa 
 
-
+########### mvtec and mvtc loco
 class ImageDataset_mvtec(Dataset):
     def __init__(self, args, root, transforms_=None, mode='train', train_paths=None, test_paths=None):
         
@@ -117,7 +117,8 @@ class ImageDataset_mvtec(Dataset):
                     ground_truth = Image.open(filename.replace("test", "ground_truth").replace(".png", "/000.png"))                        
                 if self.args.mode=='mvtec':
                     ground_truth = Image.open(filename.replace("test", "ground_truth").replace(".png", "_mask.png"))
-                
+                # TODO check for mask if resized correctly for loco
+
                 ground_truth = self.resize_transform_loco(ground_truth)  
                 img, ground_truth = transform_test(img, ground_truth)
                 
@@ -126,6 +127,93 @@ class ImageDataset_mvtec(Dataset):
 
     def __len__(self):
         return len(self.files)
+    
+###--------------------------------------------------------------- Beantech
+class ImageDataset_beantec(Dataset):
+    def __init__(self, args, root, transforms_=None, mode='train', train_paths=None, test_paths=None):
+        
+        self.img_size = 280 * args.factor
+        self.crop_size = 256 * args.factor
+        self.args = args
+        self.mode = mode
+        if train_paths is None and test_paths is None:
+            raise ValueError("either test or train paths must be provided depending on the mode")
+        
+        self.train_paths = train_paths
+        self.test_paths = test_paths
+        
+        self.transform_train = transforms.Compose([ transforms.Resize((self.crop_size, self.crop_size), Image.BICUBIC),
+                                                transforms.Pad(int(self.crop_size/10),fill=0,padding_mode='constant'),
+                                                transforms.RandomRotation(10),
+                                                transforms.RandomCrop((self.crop_size, self.crop_size)),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225 ]) ])
+        
+        self.resize_transform_bean = transforms.Resize((self.crop_size, self.crop_size), Image.BICUBIC)
+
+        if mode == 'train':
+            self.files = train_paths
+        elif mode == 'test':
+            self.files = test_paths
+            
+    def _align_transform(self, img, mask):
+        #resize to 224
+        img = TF.resize(img, self.crop_size, Image.BICUBIC)
+        mask = TF.resize(mask, self.crop_size, Image.NEAREST)
+        #toTensor
+        img = TF.to_tensor(img)
+        mask = TF.to_tensor(mask)
+        #normalize
+        img = TF.normalize(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225 ])
+        return img, mask
+    
+    def _unalign_transform(self, img, mask):
+        #resize to 256
+        img = TF.resize(img, self.img_size, Image.BICUBIC)
+        mask = TF.resize(mask, self.img_size, Image.NEAREST)
+        #random rotation
+        angle = transforms.RandomRotation.get_params([-10, 10])
+        img = TF.rotate(img, angle, fill=(0,))
+        mask = TF.rotate(mask, angle, fill=(0,))
+        #random crop
+        i, j, h, w = transforms.RandomCrop.get_params(img, output_size=(self.crop_size, self.crop_size))
+        img = TF.crop(img, i, j, h, w)
+        mask = TF.crop(mask, i, j, h, w)
+        #toTensor
+        img = TF.to_tensor(img)
+        mask = TF.to_tensor(mask)
+        #normalize
+        img = TF.normalize(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225 ])
+        return img, mask
+
+    def __getitem__(self, index):
+        filename = self.files[index]
+        img = Image.open(filename)
+        img = img.convert('RGB')
+        img = self.resize_transform_bean(img)
+
+        if self.mode == 'train':
+            img = self.transform_train(img)            
+            return filename, img
+        
+        elif self.mode == 'test':
+            transform_test = self._unalign_transform if self.args.unalign_test else self._align_transform
+            img_size = (img.size[0], img.size[1])
+            
+            if 'ok' in os.path.split(filename)[0].split(os.sep):    
+                ground_truth =Image.new('L',(img_size[0],img_size[1]),0)
+                img, ground_truth = transform_test(img, ground_truth)
+                return filename, img, ground_truth, 0
+            else:   
+                ground_truth = Image.open(filename.replace("test", "ground_truth").replace(".bmp", ".png"))
+                ground_truth = self.resize_transform_bean(ground_truth)  
+                img, ground_truth = transform_test(img, ground_truth) 
+                # TODO check for mask if resized correctly
+                return filename, img, ground_truth, 1
+            
+    def __len__(self):
+        return len(self.files)   
     
 ## Original Loader
 class ImageDataset(Dataset):
@@ -255,38 +343,38 @@ class JsonDataset(Dataset):
         return len(self.files)
 
 # Configure dataloaders # TODO add code from dev
-def Get_dataloader(args):
+
+def get_dataloader(args):
 
     if args.mode == 'mvtec' or args.mode == 'mvtec_loco':
-        ## get dataset paths for test and train 
+        
         EXPERIMENT_LOG_PATH = os.path.join(args.results_dir,args.data_set ,
                                     f'contamination_{int(args.contamination_rate*100)}',
-                                    f'{args.exp_name}-{args.data_category}'
-                                    )
+                                    f'{args.exp_name}-{args.data_category}')
         
-        # Load if already exists
-        if os.path.exists(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json")):
+        if os.path.exists(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json")) and not args.development:
             with open(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json"), "r") as file:
                 experiment_paths = json.load(file)
+                print("sucessfully loaded experiment paths")   
+                  
         else:    
             if args.mode == 'mvtec':    
                 normal_images, validation_images, sampled_anomalies_for_train, sampled_anomalies_for_val, good_images_test, remaining_anomalies_test = get_paths_mvtec(args,verbose=True)
-
-            
+                
             if args.mode == 'mvtec_loco':
+
                 normal_images, validation_images, sampled_anomalies_for_train, sampled_anomalies_for_val, good_images_test, remaining_anomalies_test = get_paths_mvtec_loco(args,verbose=True)
+
                 
             train_paths = normal_images + sampled_anomalies_for_train
             valid_paths = validation_images + sampled_anomalies_for_val
             test_paths = good_images_test + remaining_anomalies_test
-            
-            
             experiment_paths={'train':train_paths,'test':test_paths,'valid':valid_paths,'contamination_rate':args.contamination_rate,'seed':args.seed}
-
-            os.makedirs(EXPERIMENT_LOG_PATH, exist_ok=True)
-            with open(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json"), "w") as file:
-                json.dump(experiment_paths, file)
-        
+            
+            if not args.development:
+                os.makedirs(EXPERIMENT_LOG_PATH, exist_ok=True)
+                with open(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json"), "w") as file:
+                    json.dump(experiment_paths, file)
         
         DATA_PATH=os.path.join(args.data_root,args.data_category)
         train_dataloader = DataLoader(ImageDataset_mvtec(args,DATA_PATH,mode='train',train_paths = experiment_paths['train'],test_paths = None),
@@ -296,14 +384,45 @@ def Get_dataloader(args):
                                                 batch_size=args.batch_size,shuffle=False,num_workers=1,drop_last=False)
         else:
             valid_dataloader = None
-        
-
         test_dataloader = DataLoader(ImageDataset_mvtec(args,DATA_PATH,mode='test',train_paths = None,test_paths = experiment_paths['test']),
                                                         batch_size=args.batch_size,shuffle=False,num_workers=1,drop_last=False)
     
 
     if args.mode == 'beantec':
-        pass
+        
+        EXPERIMENT_LOG_PATH = os.path.join(args.results_dir,args.data_set ,
+                                    f'contamination_{int(args.contamination_rate*100)}',
+                                    f'{args.exp_name}-{args.data_category}')
+        
+        if os.path.exists(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json")) and not args.development:
+            with open(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json"), "r") as file:
+                experiment_paths = json.load(file)     
+                
+        else:    
+            normal_images, validation_images, sampled_anomalies_for_train, sampled_anomalies_for_val, good_images_test, remaining_anomalies_test = get_paths_beantec(args,verbose=True)    
+            train_paths = normal_images + sampled_anomalies_for_train
+            valid_paths = validation_images + sampled_anomalies_for_val
+            test_paths = good_images_test + remaining_anomalies_test
+            experiment_paths={'train':train_paths,'test':test_paths,'valid':valid_paths,'contamination_rate':args.contamination_rate,'seed':args.seed}
+            
+            if not args.development:
+                os.makedirs(EXPERIMENT_LOG_PATH, exist_ok=True)
+                with open(os.path.join(EXPERIMENT_LOG_PATH,"experiment_paths.json"), "w") as file:
+                    json.dump(experiment_paths, file)
+        
+        DATA_PATH=os.path.join(args.data_root,args.data_category)
+        train_dataloader = DataLoader(ImageDataset_beantec(args,DATA_PATH,mode='train',train_paths = experiment_paths['train'],test_paths = None),
+                                                        batch_size=args.batch_size,shuffle=True,num_workers=args.n_cpu,drop_last=False)
+        if len(experiment_paths['valid']) > 0:
+            valid_dataloader = DataLoader(ImageDataset_beantec(args,DATA_PATH,mode='test',train_paths = None,test_paths = experiment_paths['valid']),
+                                                batch_size=args.batch_size,shuffle=False,num_workers=1,drop_last=False)
+        else:
+            valid_dataloader = None
+        test_dataloader = DataLoader(ImageDataset_beantec(args,DATA_PATH,mode='test',train_paths = None,test_paths = experiment_paths['test']),
+                                                        batch_size=args.batch_size,shuffle=False,num_workers=1,drop_last=False)
+        
+        
+    
     if args.mode == 'visa':
         pass        
     if args.mode == 'utrad_mvtec':
@@ -319,5 +438,4 @@ def Get_dataloader(args):
 
 
     return train_dataloader,valid_dataloader, test_dataloader
-
 
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -15,7 +15,7 @@ def stratified_sample(file_paths, class_list, total_samples,seed=123):
     categorized_paths = defaultdict(list)
     for path in file_paths:
         for class_name in class_list:
-            if class_name in path:
+            if class_name in os.path.split(path)[0].split(os.sep):
                 categorized_paths[class_name].append(path)
                 break
     
@@ -49,7 +49,7 @@ def count_files_by_class(file_paths, class_list):
     return class_counts
 
 # mvtec
-def get_paths_mvtec(args,verbose=True,):
+def get_paths_mvtec(args,verbose=True):
     
     anomaly_categories=args.dataset_parameters['anomaly_categories']
     category=args.data_category
@@ -86,7 +86,7 @@ def get_paths_mvtec(args,verbose=True,):
         sampled_anomalies_for_val = []
         validation_images = []
 
-
+    print("debug","got to load the data")
     if verbose:
         print(f'category: {category}, normals train:  {len(normal_images)}, anomalies test: {len(anomaly_images_test)}, normal test: {len(good_images_test)}')       
         print(f'anomalies test total:     {count_files_by_class(anomaly_images_test, anomaly_categories[category])}')
@@ -94,7 +94,6 @@ def get_paths_mvtec(args,verbose=True,):
         print(f'anomalies test remaining: {count_files_by_class(remaining_anomalies_test, anomaly_categories[category])}')
     
     return normal_images, validation_images, sampled_anomalies_for_train, sampled_anomalies_for_val, good_images_test, remaining_anomalies_test
-
 
 def get_paths_mvtec_loco(args,verbose=True):
     
@@ -140,4 +139,48 @@ def get_paths_mvtec_loco(args,verbose=True):
         print(f'anomalies test sampled:   {count_files_by_class(sampled_anomalies_for_train, anomaly_categories[category])}')
         print(f'anomalies test remaining: {count_files_by_class(remaining_anomalies_test, anomaly_categories[category])}')
         
+    return normal_images, validation_images, sampled_anomalies_for_train, sampled_anomalies_for_val, good_images_test, remaining_anomalies_test
+
+
+def get_paths_beantec(args,verbose=True):
+    
+    anomaly_categories=args.dataset_parameters['anomaly_categories']
+    category=args.data_category
+    validation=args.dataset_parameters['use_validation']
+    validation_split=args.dataset_parameters['validation_split']
+    DATA_PATH=args.data_root
+    
+    NORMAL_PATH = os.path.join(DATA_PATH, f'{category}/train/ok')
+    ANOMALY_PATH = os.path.join(DATA_PATH , f'{category}/test')
+    
+        
+    normal_images=[os.path.join(NORMAL_PATH,item) for item in os.listdir(NORMAL_PATH)]
+    file_path = []
+    for root, _, files in os.walk(ANOMALY_PATH):
+        for file in files:
+            file_path.append( os.path.join(root, file))
+        
+    anomaly_images_test=[item for item in file_path if "ok" not in os.path.split(item)[0].split(os.sep)]
+    good_images_test=[item for item in file_path if "ok" in os.path.split(item)[0].split(os.sep)]
+    
+
+    
+    n_samples = int(len(normal_images)*args.contamination_rate)
+    sampled_anomalies_for_train, remaining_anomalies_test = stratified_sample(anomaly_images_test, anomaly_categories[category], n_samples, args.seed)
+
+    if validation_split > 0:
+        if validation!= True:
+            raise ValueError('validation_split is set to > 0 but use_validation is set to False')
+        normal_images,validation_images = train_test_split(normal_images, test_size=validation_split, random_state=args.seed)
+        sampled_anomalies_for_train, sampled_anomalies_for_val = train_test_split(sampled_anomalies_for_train, test_size=validation_split, random_state=args.seed)
+    else:
+        sampled_anomalies_for_val = []
+        validation_images = []
+        
+    if verbose:
+        print(f'category: {category}, normals train:  {len(normal_images)}, anomalies test: {len(anomaly_images_test)}, normal test: {len(good_images_test)}')       
+        print(f'anomalies test total:     {count_files_by_class(anomaly_images_test, anomaly_categories[category])}')
+        print(f'anomalies test sampled:   {count_files_by_class(sampled_anomalies_for_train, anomaly_categories[category])}')
+        print(f'anomalies test remaining: {count_files_by_class(remaining_anomalies_test, anomaly_categories[category])}')
+    
     return normal_images, validation_images, sampled_anomalies_for_train, sampled_anomalies_for_val, good_images_test, remaining_anomalies_test
