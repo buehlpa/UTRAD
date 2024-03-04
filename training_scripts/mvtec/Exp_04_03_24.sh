700c40328f2524f129ff8671662b559f99372732
Pascal <buehler1991@gmail.com>
2024-03-04 15:54:40 +0100

    training scripüts

---
+++ training_scripts/mvtec/Exp_04_03_24.sh
@@ -0,0 +1,110 @@
+#!/bin/bash  Exp_04_03_24_run_1
+
+python main.py --exp_name Exp_04_03_24_run_1 --data_category bottle --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_1 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_1 --data_category cable --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_1 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_1 --data_category screw --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_1 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+python valid.py --exp_name Exp_04_03_24_run_1 --data_category bottle --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_1 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_1 --data_category cable --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_1 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_1 --data_category screw --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_1 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+
+
+
+
+python main.py --exp_name Exp_04_03_24_run_2 --data_category bottle --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_2 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_2 --data_category cable --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_2 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_2 --data_category screw --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_2 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+python valid.py --exp_name Exp_04_03_24_run_2 --data_category bottle --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_2 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_2 --data_category cable --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_2 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_2 --data_category screw --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_2 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+
+
+python main.py --exp_name Exp_04_03_24_run_3 --data_category bottle --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_3 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_3 --data_category cable --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_3 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_3 --data_category screw --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_3 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+python valid.py --exp_name Exp_04_03_24_run_3 --data_category bottle --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_3 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_3 --data_category cable --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_3 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_3 --data_category screw --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_3 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+
+
+python main.py --exp_name Exp_04_03_24_run_4 --data_category bottle --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_4 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_4 --data_category cable --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_4 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+python valid.py --exp_name Exp_04_03_24_run_4 --data_category bottle --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_4 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_4 --data_category cable --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_4 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+
+
+python main.py --exp_name Exp_04_03_24_run_5 --data_category bottle --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_5 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_5 --data_category cable --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_5 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python main.py --exp_name Exp_04_03_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.0
+python main.py --exp_name Exp_04_03_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1
+
+
+python valid.py --exp_name Exp_04_03_24_run_5 --data_category bottle --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_5 --data_category bottle --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_5 --data_category cable --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_5 --data_category cable --mode mvtec --contamination_rate 0.1
+
+python valid.py --exp_name Exp_04_03_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.0
+python valid.py --exp_name Exp_04_03_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1
