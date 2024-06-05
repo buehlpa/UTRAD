fa98dcd49a9561a5438d44c20e40012c731740bb
Pascal <buehler1991@gmail.com>
2024-06-05 11:27:06 +0200

    added extensive runs

---
+++ training_scripts/mvtec/Exp_04_06_24_all3.sh
@@ -0,0 +1,27 @@
+python main.py --exp_name Exp_04_06_24_run_1 --data_category bottle --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_2 --data_category bottle --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_3 --data_category bottle --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_4 --data_category bottle --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_5 --data_category bottle --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+
+python main.py --exp_name Exp_04_06_24_run_1 --data_category cable --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_2 --data_category cable --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_3 --data_category cable --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_4 --data_category cable --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_5 --data_category cable --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+
+python main.py --exp_name Exp_04_06_24_run_1 --data_category metal_nut --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_2 --data_category metal_nut --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_3 --data_category metal_nut --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_4 --data_category metal_nut --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+python main.py --exp_name Exp_04_06_24_run_5 --data_category metal_nut --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+
+
+python main.py --exp_name Exp_04_06_24_run_1 --data_category grid --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+
+
+python main.py --exp_name Exp_04_06_24_run_4 --data_category toothbrush --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
+
+
+
+python main.py --exp_name Exp_04_06_24_run_1 --data_category pill --mode mvtec --contamination_rate 0.0 --fixed_seed_bool False
--- training_scripts/mvtec_loco/Exp_04_06_24_all2.sh
+++ training_scripts/mvtec_loco/Exp_04_06_24_all2.sh
@@ -1,13 +1,1 @@
-python main.py --exp_name Exp_04_06_24_run_1 --data_category splicing_connectors   --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_2 --data_category splicing_connectors   --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_3 --data_category splicing_connectors   --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_4 --data_category splicing_connectors   --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_5 --data_category splicing_connectors   --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-
-python main.py --exp_name Exp_04_06_24_run_1 --data_category breakfast_box         --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_2 --data_category breakfast_box         --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_3 --data_category breakfast_box         --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_4 --data_category breakfast_box         --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-python main.py --exp_name Exp_04_06_24_run_5 --data_category breakfast_box         --mode mvtec_loco   --data_set mvtec_loco  --data_root /home/bule/projects/datasets/mvtec_loco_anomaly_detection --contamination_rate 0.0 --fixed_seed_bool False
-
 
