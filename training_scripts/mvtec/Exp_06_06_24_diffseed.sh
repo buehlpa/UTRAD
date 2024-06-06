d20e81fbe5be73b8929db4e2360703aa20a9264d
Pascal <buehler1991@gmail.com>
2024-06-06 09:15:46 +0200

    added bash to run later

---
+++ training_scripts/mvtec/Exp_06_06_24_diffseed.sh
@@ -0,0 +1,21 @@
+python main.py        --exp_name Exp_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+python trainscores.py --exp_name Exp_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+python testscores.py  --exp_name Exp_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+python cleantrain.py  --exp_name Exp_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+
+python main.py        --exp_name Exp_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+python trainscores.py --exp_name Exp_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+python testscores.py  --exp_name Exp_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+python cleantrain.py  --exp_name Exp_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858561939
+
+
+
+python main.py        --exp_name Exp_diffseed_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+python trainscores.py --exp_name Exp_diffseed_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+python testscores.py  --exp_name Exp_diffseed_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+python cleantrain.py  --exp_name Exp_diffseed_03_05_24_run_4 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+
+python main.py        --exp_name Exp_diffseed_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+python trainscores.py --exp_name Exp_diffseed_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+python testscores.py  --exp_name Exp_diffseed_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
+python cleantrain.py  --exp_name Exp_diffseed_03_05_24_run_5 --data_category screw --mode mvtec --contamination_rate 0.1 --fixed_seed_TEST_bool False --seed 858539111
