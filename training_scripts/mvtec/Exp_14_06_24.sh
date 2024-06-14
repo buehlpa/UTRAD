1f55013c3ba1c99b6499d44d53fb67740979bc13
Pascal <buehler1991@gmail.com>
2024-06-14 12:09:46 +0200

    added sets

---
+++ training_scripts/mvtec/Exp_14_06_24.sh
@@ -0,0 +1,25 @@
+
+python main.py        --exp_name Exp_FewData_14_06_24_run_1 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python trainscores.py --exp_name Exp_FewData_14_06_24_run_1 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python testscores.py  --exp_name Exp_FewData_14_06_24_run_1 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python cleantrain.py  --exp_name Exp_FewData_14_06_24_run_1 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+
+python main.py        --exp_name Exp_FewData_14_06_24_run_2 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python trainscores.py --exp_name Exp_FewData_14_06_24_run_2 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python testscores.py  --exp_name Exp_FewData_14_06_24_run_2 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python cleantrain.py  --exp_name Exp_FewData_14_06_24_run_2 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+
+python main.py        --exp_name Exp_FewData_14_06_24_run_3 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python trainscores.py --exp_name Exp_FewData_14_06_24_run_3 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python testscores.py  --exp_name Exp_FewData_14_06_24_run_3 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python cleantrain.py  --exp_name Exp_FewData_14_06_24_run_3 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+
+python main.py        --exp_name Exp_FewData_14_06_24_run_4 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python trainscores.py --exp_name Exp_FewData_14_06_24_run_4 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python testscores.py  --exp_name Exp_FewData_14_06_24_run_4 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python cleantrain.py  --exp_name Exp_FewData_14_06_24_run_4 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+
+python main.py        --exp_name Exp_FewData_14_06_24_run_5 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python trainscores.py --exp_name Exp_FewData_14_06_24_run_5 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python testscores.py  --exp_name Exp_FewData_14_06_24_run_5 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
+python cleantrain.py  --exp_name Exp_FewData_14_06_24_run_5 --data_category leather --mode mvtec --contamination_rate 0.1 --data_ratio 0.5
