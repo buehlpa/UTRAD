b70f64bdb3a4384c78202d95ae7178f0d7450584
Pascal <buehler1991@gmail.com>
2024-07-20 15:04:06 +0200

    method to remove contam

--- training_scripts/visa/halfdataa_11_07_24_pipe_fryum.sh
+++ training_scripts/visa/halfdataa_11_07_24_pipe_fryum.sh
@@ -1,6 +1,4 @@
 
-python main.py       --exp_name Exp_11_07_24_run_1 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python cleantrain.py --exp_name Exp_11_07_24_run_1 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
 
 python main.py       --exp_name Exp_11_07_24_run_2 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
 python cleantrain.py --exp_name Exp_11_07_24_run_2 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250
@@ -8,16 +6,3 @@ python cleantrain.py --exp_name Exp_11_07_24_run_2 --data_category pipe_fryum --
 python main.py       --exp_name Exp_11_07_24_run_3 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
 python cleantrain.py --exp_name Exp_11_07_24_run_3 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250
 
-python main.py       --exp_name Exp_11_07_24_run_4 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python cleantrain.py --exp_name Exp_11_07_24_run_4 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250
-
-python main.py       --exp_name Exp_11_07_24_run_5 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python cleantrain.py --exp_name Exp_11_07_24_run_5 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA --contamination_rate 0.1   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250
-
-
-# clean 
-python main.py   --exp_name Exp_11_07_24_run_1 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python main.py   --exp_name Exp_11_07_24_run_2 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python main.py   --exp_name Exp_11_07_24_run_3 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python main.py   --exp_name Exp_11_07_24_run_4 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
-python main.py   --exp_name Exp_11_07_24_run_5 --data_category pipe_fryum --mode visa  --data_set visa --data_root /home/bule/projects/datasets/VisA   --batch_size 2  --fixed_seed_bool False --fixed_n_normals 250 
