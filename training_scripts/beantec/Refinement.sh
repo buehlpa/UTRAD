e51311ac4db31bcc69698b8d795eb17740be537f
Pascal <buehler1991@gmail.com>
2024-10-23 10:02:59 +0200

    last changes

--- training_scripts/beantec/Refinement.sh
+++ training_scripts/beantec/Refinement.sh
@@ -1,49 +1,49 @@
 toothbrush
 # !/bin/bash
 
-# python refinement_featurewise_beantec.py  --category 1 --run 1 --mode own
-# python refinement_featurewise_beantec.py  --category 2 --run 1 --mode own
+python refinement_featurewise_beantec.py  --category 1 --run 1 --mode own
+python refinement_featurewise_beantec.py  --category 2 --run 1 --mode own
 python refinement_featurewise_beantec.py  --category 3 --run 1 --mode own
 
 
 
-# python refinement_featurewise_beantec.py  --category 1 --run 1 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 1 --run 2 --mode vit_imagenet 
-# python refinement_featurewise_beantec.py  --category 1 --run 3 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 1 --run 4 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 1 --run 5 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 1 --run 1 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 1 --run 2 --mode vit_imagenet 
+python refinement_featurewise_beantec.py  --category 1 --run 3 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 1 --run 4 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 1 --run 5 --mode vit_imagenet
 
 
-# python refinement_featurewise_beantec.py  --category 2 --run 1 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 2 --run 2 --mode vit_imagenet 
-# python refinement_featurewise_beantec.py  --category 2 --run 3 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 2 --run 4 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 2 --run 5 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 2 --run 1 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 2 --run 2 --mode vit_imagenet 
+python refinement_featurewise_beantec.py  --category 2 --run 3 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 2 --run 4 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 2 --run 5 --mode vit_imagenet
 
-# python refinement_featurewise_beantec.py  --category 3 --run 1 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 3 --run 2 --mode vit_imagenet 
-# python refinement_featurewise_beantec.py  --category 3 --run 3 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 3 --run 4 --mode vit_imagenet
-# python refinement_featurewise_beantec.py  --category 3 --run 5 --mode vit_imagenet
-
-
+python refinement_featurewise_beantec.py  --category 3 --run 1 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 3 --run 2 --mode vit_imagenet 
+python refinement_featurewise_beantec.py  --category 3 --run 3 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 3 --run 4 --mode vit_imagenet
+python refinement_featurewise_beantec.py  --category 3 --run 5 --mode vit_imagenet
 
 
-# python refinement_featurewise_beantec.py  --category 1 --run 1 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 1 --run 2 --mode vit_beans 
-# python refinement_featurewise_beantec.py  --category 1 --run 3 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 1 --run 4 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 1 --run 5 --mode vit_beans
 
-# python refinement_featurewise_beantec.py  --category 2 --run 1 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 2 --run 2 --mode vit_beans 
-# python refinement_featurewise_beantec.py  --category 2 --run 3 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 2 --run 4 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 2 --run 5 --mode vit_beans
 
-# python refinement_featurewise_beantec.py  --category 3 --run 1 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 3 --run 2 --mode vit_beans 
-# python refinement_featurewise_beantec.py  --category 3 --run 3 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 3 --run 4 --mode vit_beans
-# python refinement_featurewise_beantec.py  --category 3 --run 5 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 1 --run 1 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 1 --run 2 --mode vit_beans 
+python refinement_featurewise_beantec.py  --category 1 --run 3 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 1 --run 4 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 1 --run 5 --mode vit_beans
+
+python refinement_featurewise_beantec.py  --category 2 --run 1 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 2 --run 2 --mode vit_beans 
+python refinement_featurewise_beantec.py  --category 2 --run 3 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 2 --run 4 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 2 --run 5 --mode vit_beans
+
+python refinement_featurewise_beantec.py  --category 3 --run 1 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 3 --run 2 --mode vit_beans 
+python refinement_featurewise_beantec.py  --category 3 --run 3 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 3 --run 4 --mode vit_beans
+python refinement_featurewise_beantec.py  --category 3 --run 5 --mode vit_beans
 
